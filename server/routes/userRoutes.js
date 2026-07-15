5231d575f82259bd2f4f589954fd48565478d0bd
Bhanodhar <bhanodharpb@gmail.com>
2026-07-15 12:11:51 +0530

    add: feature users manage

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {registerUser, loginUser} = require("../controllers/userAuthController");
+const { getAllUsers, deleteUser } = require("../controllers/userController");
 const {protect, userOnly} = require("../middleware/authMiddleware");
 
 router.post('/register',registerUser);
@@ -9,5 +10,10 @@ router.post('/login',loginUser);
 router.get('/profile',protect, userOnly, (req,res)=>{
     res.json({message:"Welcome User!",user:req.user});
 });
+
+
+// Admin only routes
+router.get("/all", protect, adminOnly, getAllUsers);
+router.delete("/:id", protect, adminOnly, deleteUser);
 
 module.exports = router;
